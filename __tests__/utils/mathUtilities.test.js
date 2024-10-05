1aa593889a638f7a76499c7022cc9f26ec6e3461
Victoria Breen <victoriabreen8@hotmail.com>
2024-10-05 18:44:05 -0230

    Ensuring the pages are connecting

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,9 @@
-const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
+// needs to include
+//  a question needs to be properly generated
+// correct and incorrect answer needs to be detected
 
-describe("Tests for getQuestion", () => {
+const { CorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
-});
+describe("Tests for getQuestion", () => {});
 
-describe("Tests for isCorrectAnswer", () => {
-    
-});
+describe("Tests for isCorrectAnswer", () => {});
